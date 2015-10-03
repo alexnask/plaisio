72fd10908bc75a1ab2cc8a56de924c0020b2a720
Alexandros Naskos <alex_naskos@hotmail.com>
2015-10-03 22:35:36 +0300

    Forgot to add cpp file

---
+++ source/Force.cpp
@@ -0,0 +1,41 @@
+#include <plaisio/Force.hpp>
+
+#include <cassert>
+
+namespace plaisio {
+    Force::Force(double _x, double _z, double _angle, double _magnitude) : x(_x), z(_z), angle(_angle), magnitude(_magnitude) {}
+
+    Force Force::operator - () const {
+        return { x, z, angle, -magnitude };
+    }
+
+    Force Force::operator + (const Force& other) const {
+        assert(x == other.x && z == other.z);
+
+        auto pair1 = components();
+        auto pair2 = other.components();
+
+        double xMagn = pair1.first.magnitude + pair2.first.magnitude;
+        double yMagn = pair2.second.magnitude + pair2.second.magnitude;
+
+        double nAngle = xMagn == 0 ? M_PI / 2 : atan2(yMagn, xMagn);
+
+        return { x, z, nAngle,  sqrt(xMagn * xMagn + yMagn * yMagn)};
+    }
+
+    Force Force::operator - (const Force& other) const {
+        return *this + (-other);
+    }
+
+    std::pair<Force, Force> Force::components(double xAngle, double zAngle) const {
+        assert(abs(xAngle - zAngle) == M_PI/2);
+
+        double newX = -magnitude * cos(xAngle);
+        double newZ = magnitude * sin(zAngle);
+        if (xAngle - zAngle < 0) {
+            newZ = -newZ;
+        }
+
+        return { { x, z, xAngle, newX}, { x, z, zAngle, newZ } };
+    }
+}
